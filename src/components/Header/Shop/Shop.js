d573e433960d95da0785401b5061089ff67f8239
Rabeyahabiba <hema.rabeya93@gmail.com>
2021-03-07 19:18:46 +0600

    added last

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,40 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import fakeData from '../../../fakeData';
-import { useState} from 'react';
+import { useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../../Cart/Cart';
-import { addToDatabaseCart } from '../../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart } from '../../../utilities/databaseManager';
+import { Link } from 'react-router-dom';
+
+
 const Shop = () => {
-    const first10 = fakeData.slice(0,10);
+    const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
-    const [cart, setCart] =useState([]);
-    const handleAddProduct =(product) =>{
-        const newCart =[...cart, product];
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const previousCart = productKeys.map(existingKey => {
+            const product = fakeData.find(pd => pd.key === existingKey);
+            product.quantity = savedCart[existingKey];
+            console.log(existingKey, savedCart[existingKey]);
+            return product;
+        })
+        setCart(previousCart);
+    })
+    const handleAddProduct = (product) => {
+        const toBeaddedKey = product.key;
+        const sameProduct = cart.find(pd => pd.key === toBeaddedKey);
+        let count = 1;
+        let newCart;
+        if (sameProduct) {
+            count = sameProduct.quantity + 1;
+            sameProduct.quantity = count;
+            const others = cart.filter(pd => pd.key !== toBeaddedKey)
+            newCart = [...others, sameProduct];
+
+        }
+        else {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
         setCart(newCart);
-        const sameProduct = newCart.filter (pd => pd.key === product.key);
-        const count = sameProduct.length;
         addToDatabaseCart(product.key, count);
     }
     return (
-        <div className= "shop-container">
-         <div className ="product-container">
+        <div className="twin-container">
+            <div className="product-container">
 
-               {
-                   products.map(pd => <Product
-                   key ={pd.key}
-                   showAddToCart = {true}
-                    handleAddProduct ={handleAddProduct}
-                    product = {pd}>
+                {
+                    products.map(pd => <Product
+                        key={pd.key}
+                        showAddToCart={true}
+                        handleAddProduct={handleAddProduct}
+                        product={pd}>
                     </Product>)
-               }
-           
-           </div>
-           <div className ="cart-container">
-               <Cart cart={cart}></Cart>
-               
+                }
 
-          </div> 
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+                <Link to="/review">
+                    <button className="main-button"> Review Order </button>
+                </Link>
+            </div>
         </div>
     );
 };
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,30 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import ReviewItem from '../ReviewItem/ReviewItem';
-
+import Cart from '../Cart/Cart';
+import happyImage from '../../images/giphy.gif';
 const Review = () => {
-    const [cart, setCart] = useState ([]) ;
+    const [cart, setCart] = useState([]);
+    const [orderPlaced, setOrderPlaced] = useState (false);
+    const handlePlaceOrder = () => {
+        setCart ([]) ;
+        setOrderPlaced(true);
+        processOrder();
+        // console.log('order placed');
+    }
+    const removeProduct = (productKey) => {
+        // console.log ('remove clicked', productKey);
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
 
-    useEffect ( ()=> {
-            const savedCart = getDatabaseCart();
-            const productKeys = Object.keys (savedCart);
+    useEffect(() => {
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
 
-            const cartProducts =  productKeys.map (key => {
-                const product = fakeData.find( pd => pd.key === key);
-                product.quantity = savedCart [key] ;
-                return product;
-            });
-            setCart (cartProducts);
-    }, [])
+        const cartProducts = productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        setCart(cartProducts);
+    }, []);
+     let thankyou;
+     if(orderPlaced){
+        thankyou = <img src={happyImage} alt ="" />
+     }
     return (
-        <div>
-            <h1> Cart Items:{cart.length}</h1>
-            {
-                 cart.map (pd => <ReviewItem 
-                    key = {pd.key}
-                    product = {pd}></ReviewItem>)
-            }
+        <div ClassName="twin-container">
+           
+            <div className="product-container">
+                {
+                    cart.map(pd => <ReviewItem
+                    key={pd.key}
+                    removeProduct={removeProduct}
+                    product={pd}></ReviewItem>)
+
+                }
+                {
+                    thankyou
+                }
+            </div>
+
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick = {handlePlaceOrder} className ="main-button">Place Order</button>
+                </Cart>
+            </div>
         </div>
     );
 };
